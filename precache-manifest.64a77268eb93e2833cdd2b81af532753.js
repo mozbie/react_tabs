f8a087f691c9c75b0d5a75373d3360b0b1df408e
Alina Kizilova <alinakzilova@gmail.com>
2019-09-13 10:11:43 +0300

    Updates

---
+++ precache-manifest.64a77268eb93e2833cdd2b81af532753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae1c27b22f01d9342162d785ad2b51a",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "436122428f99e31560c6",
+    "url": "/react_tabs/static/css/main.e8540c6e.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_tabs/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "436122428f99e31560c6",
+    "url": "/react_tabs/static/js/main.d3c8082f.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
